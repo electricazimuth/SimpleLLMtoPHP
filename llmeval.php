5b12e4eff8c93ed0c74b449a9d9625c1e478d025
electricazimuth <david@electric-azimuth.co.uk>
2024-02-05 18:00:20 +0000

    command line fluff

--- llmeval.php
+++ llmeval.php
@@ -8,66 +8,84 @@ require("bootstrapper.inc.php");
 $logname = 'llmeval.00';
 $do_save_to_db = true;
 
-$q = "SELECT l.lyrics, l.id, l.artist, l.title, p.pri_key FROM lyrics_hot100 l, processing p WHERE p.stage = 1 AND p.lyric_id = l.id ORDER BY p.pri_key ASC LIMIT 100";
+
 
 
 //1202  2332 1182
 // l.row_id ASC LIMIT 1,1";
+$test_q = "SELECT COUNT(*) as counter FROM processing WHERE stage = 1";
+$test_rows = $registry->db->getRows($test_q);
+$rows_to_do = (int)$test_rows[0]['counter'];
 
-$rows = $registry->db->getRows($q);
+while($rows_to_do > 200){
 
-$registry->llm->SetMemory(
-'I want you to act as a song lyric checker. You are checking lyrics for any problematic language or themes. You will read through the lyrics and assess the swearing used and the offensive and provocative nature of the contents of the lyrics.
-I would like you to respond with numerical ratings from 0 to 10 for any swearing and the lyrics offensiveness and provocativeness. 
-A swearing rating of 10 is when theres a lot of strong swearing, swear words from high to low score; "cunt", "nigger", "piss", "pussy", "fuck", "dick", "cock", "shit", "twat", "bollocks" and "wanker" the least. 
-An offensive rating of 10 is for something that most people would be offended by. 
-A provocative rating of 10 is for lyrics which are overtly provocative. Strong swear words like "cunt" would score a 10 for offensiveness and swearing.
-Your response should contain only "swearing:", "offensive:" and "provacative:" followed by a number. 
+    $q = "SELECT l.lyrics, l.id, l.artist, l.title, p.pri_key FROM lyrics_hot100 l, processing p WHERE p.stage = 1 AND p.lyric_id = l.id ORDER BY p.pri_key ASC LIMIT 200";
+    $rows = $registry->db->getRows($q);
 
-IMPORTANT remember to only respond with the "swearing", "offensive" and "provocative" numerical ratings. Nothing else. No summaries, references, calculations or explainations. 
-Check the following lyrics and respond with your ratings:
+    $registry->llm->SetMemory(
+    'I want you to act as a song lyric checker. You are checking lyrics for any problematic language or themes. You will read through the lyrics and assess the swearing used and the offensive and provocative nature of the contents of the lyrics.
+    I would like you to respond with numerical ratings from 0 to 10 for any swearing and the lyrics offensiveness and provocativeness. 
+    A swearing rating of 10 is when theres a lot of strong swearing, swear words from high to low score; "cunt", "nigger", "piss", "pussy", "fuck", "dick", "cock", "shit", "twat", "bollocks" and "wanker" the least. 
+    An offensive rating of 10 is for something that most people would be offended by. 
+    A provocative rating of 10 is for lyrics which are overtly provocative. Strong swear words like "cunt" would score a 10 for offensiveness and swearing.
+    Your response should contain only "swearing:", "offensive:" and "provacative:" followed by a number. 
 
-# LYRICS
-'
-);
-/*
+    IMPORTANT remember to only respond with the "swearing", "offensive" and "provocative" numerical ratings. Nothing else. No summaries, references, calculations or explainations. 
+    Check the following lyrics and respond with your ratings:
 
-An example response follows:
+    # LYRICS
+    '
+    );
+    /*
 
-swearing: {swearing rating}
-offensive: {offensive rating}
-provocative: {provocative rating}
+    An example response follows:
 
-
-# Example:
-swearing: {swearing rating}
-offensive: {offensive rating}
-provocative: {provocative rating} 
-*/
+    swearing: {swearing rating}
+    offensive: {offensive rating}
+    provocative: {provocative rating}
 
 
-$registry->llm->SetPromptFormat(PromptFormat::Alpaca);//VicunaShort ); Mistral //mixtral,llongorca - ChatML, Laser - Ollama , Alpaca -pivot moe LlamaChat Vicuna   MistralStopper MPT
+    # Example:
+    swearing: {swearing rating}
+    offensive: {offensive rating}
+    provocative: {provocative rating} 
+    */
 
-$registry->llm->SetMaxLength(100);
 
-if( is_array($rows) && count($rows) > 0 ){
-    foreach($rows as $row){
-        $reply = $registry->llm->Generate($row['lyrics']);
+    $registry->llm->SetPromptFormat(PromptFormat::Alpaca);//VicunaShort ); Mistral //mixtral,llongorca - ChatML, Laser - Ollama , Alpaca -pivot moe LlamaChat Vicuna   MistralStopper MPT
 
-        if( $do_save_to_db ){
-            $q_reply = ( strlen($reply) > 254 ) ? substr($reply , 0,254) : $reply;
-            $q = "UPDATE processing SET llm_eval = ?, stage = 2 WHERE pri_key = ?";
-            $registry->db->sendQueryP($q, array($q_reply, $row['pri_key']), "si");
+    $registry->llm->SetMaxLength(100);
+
+    if( is_array($rows) && count($rows) > 0 ){
+        foreach($rows as $row){
+            $reply = $registry->llm->Generate($row['lyrics']);
+            //!todo
+            //check reply format - retry if its formatted incorrectly
+
+            if( $do_save_to_db ){
+                $q_reply = ( strlen($reply) > 254 ) ? substr($reply , 0,254) : $reply;
+                $q = "UPDATE processing SET llm_eval = ?, stage = 2 WHERE pri_key = ?";
+                $registry->db->sendQueryP($q, array($q_reply, $row['pri_key']), "si");
+            }
         }
     }
-}
-$loginfo = array();
-$loginfo['model'] = $registry->llm->GetModel();
-$loginfo['totaltime'] = microtime(true) - $time_start;
-$loginfo['numrows'] = count($rows);
-$loginfo['avetime'] = number_format((float)($end_time / count($rows)), 2, '.', '');
 
-Utils::DbLog($logname, json_encode($loginfo,JSON_FORCE_OBJECT | JSON_PRETTY_PRINT), $registry->db );
+    $test_rows = $registry->db->getRows($test_q);
+    $rows_to_do = (int)$test_rows[0]['counter'];
+
+
+    $loginfo = array();
+    $loginfo['model'] = $registry->llm->GetModel();
+    $loginfo['totaltime'] = microtime(true) - $time_start;
+    $loginfo['numrows'] = count($rows);
+    $loginfo['avetime'] = number_format((float)($loginfo['totaltime'] / count($rows)), 2, '.', '');
+    $loginfo['todo'] = $rows_to_do;
+
+    Utils::DbLog($logname, json_encode($loginfo,JSON_FORCE_OBJECT | JSON_PRETTY_PRINT), $registry->db );
+
+    
+
+}
 
 //include('templates/footer.inc.php');
 echo 'done';
