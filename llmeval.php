368be2bbb2615d061aa55a06cc7593cecd661ae2
electricazimuth <david@electric-azimuth.co.uk>
2024-02-06 13:52:32 +0000

    reprocesser

--- llmeval.php
+++ llmeval.php
@@ -8,7 +8,31 @@ require("bootstrapper.inc.php");
 $logname = 'llmeval.00';
 $do_save_to_db = true;
 
+$processing_prompt = 'I want you to act as a song lyric checker. You are checking lyrics for any problematic language or themes. You will read through the lyrics and assess the swearing used and the offensive and provocative nature of the contents of the lyrics.
+I would like you to respond with numerical ratings from 0 to 10 for any swearing and the lyrics offensiveness and provocativeness. 
+A swearing rating of 10 is when theres a lot of strong swearing, swear words from high to low score; "cunt", "nigger", "piss", "pussy", "fuck", "dick", "cock", "shit", "twat", "bollocks" and "wanker" the least. 
+An offensive rating of 10 is for something that most people would be offended by. 
+A provocative rating of 10 is for lyrics which are overtly provocative. Strong swear words like "cunt" would score a 10 for offensiveness and swearing.
+Your response should contain only "swearing:", "offensive:" and "provacative:" followed by a number. 
 
+IMPORTANT remember to only respond with the "swearing", "offensive" and "provocative" numerical ratings. Nothing else. No summaries, references, calculations or explainations. 
+Check the following lyrics and respond with your ratings:
+
+# LYRICS
+';
+
+$reprocessing_prompt = 'I want you to act as a song lyric checker. You are checking lyrics for any problematic language or themes. You will read through the lyrics and assess the swearing used and the offensive and provocative nature of the contents of the lyrics.
+I would like you to respond with numerical ratings from 0 to 10 for any swearing and the lyrics offensiveness and provocativeness. 
+A swearing rating of 10 is when theres a lot of strong swearing, swear words from high to low score; "cunt", "nigger", "piss", "pussy", "fuck", "dick", "cock", "shit", "twat", "bollocks" and "wanker" the least. 
+An offensive rating of 10 is for something that most people would be offended by. 
+A provocative rating of 10 is for lyrics which are overtly provocative. Strong swear words like "cunt" would score a 10 for offensiveness and swearing.
+Your response should contain "swearing:", "offensive:" and "provacative:" followed by a number. 
+
+IMPORTANT remember to respond with the word "swearing", "offensive" and "provocative" followed by a numerical ratings. Nothing else. No summaries, references, calculations or explainations. 
+Check the following lyrics and respond with your ratings:
+
+# LYRICS
+';
 
 
 //1202  2332 1182
@@ -24,20 +48,7 @@ while($rows_to_do > 10){
     $q = "SELECT l.lyrics, l.id, l.artist, l.title, p.pri_key FROM lyrics_hot100 l, processing p WHERE p.stage = 1 AND p.lyric_id = l.id ORDER BY p.pri_key ASC LIMIT 200";
     $rows = $registry->db->getRows($q);
 
-    $registry->llm->SetMemory(
-    'I want you to act as a song lyric checker. You are checking lyrics for any problematic language or themes. You will read through the lyrics and assess the swearing used and the offensive and provocative nature of the contents of the lyrics.
-    I would like you to respond with numerical ratings from 0 to 10 for any swearing and the lyrics offensiveness and provocativeness. 
-    A swearing rating of 10 is when theres a lot of strong swearing, swear words from high to low score; "cunt", "nigger", "piss", "pussy", "fuck", "dick", "cock", "shit", "twat", "bollocks" and "wanker" the least. 
-    An offensive rating of 10 is for something that most people would be offended by. 
-    A provocative rating of 10 is for lyrics which are overtly provocative. Strong swear words like "cunt" would score a 10 for offensiveness and swearing.
-    Your response should contain only "swearing:", "offensive:" and "provacative:" followed by a number. 
-
-    IMPORTANT remember to only respond with the "swearing", "offensive" and "provocative" numerical ratings. Nothing else. No summaries, references, calculations or explainations. 
-    Check the following lyrics and respond with your ratings:
-
-    # LYRICS
-    '
-    );
+    $registry->llm->SetMemory($reprocessing_prompt);
     /*
 
     An example response follows:
@@ -63,8 +74,9 @@ while($rows_to_do > 10){
             $reply = $registry->llm->Generate($row['lyrics']);
             //!todo
             //check reply format - retry if its formatted incorrectly
-
+            
             if( $do_save_to_db ){
+                $reply = trim($reply);
                 $q_reply = ( strlen($reply) > 254 ) ? substr($reply , 0,254) : $reply;
                 $q = "UPDATE processing SET llm_eval = ?, stage = 2 WHERE pri_key = ?";
                 $registry->db->sendQueryP($q, array($q_reply, $row['pri_key']), "si");
